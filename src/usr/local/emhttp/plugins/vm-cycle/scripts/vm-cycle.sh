ff43fe156f21e2c3f93418f64da4b1356588ba3b
alturismo <alturismo@gmail.com>
2024-11-30 18:41:36 +0100

    update

--- src/usr/local/emhttp/plugins/vm-cycle/scripts/vm-cycle.sh
+++ src/usr/local/emhttp/plugins/vm-cycle/scripts/vm-cycle.sh
@@ -13,38 +13,40 @@ vm2_state=$(virsh domstate "$VM2")
 echo "$VM1 is $vm1_state"
 echo "$VM2 is $vm2_state"
 
+sleep 600
+
 if [ "$vm1_state" = "$vm_running" ] && [ "$vm2_state" = "$vm_down" ]; then
-	echo "$VM1 is running shutting down"
-	virsh shutdown "$VM1"
-	vm1_new_state=$(virsh domstate "$VM1")
-	until [ "$vm1_new_state" = "$vm_down" ]; do
-		echo "$VM1 $vm1_new_state"
-		vm1_new_state=$(virsh domstate "$VM1")
-		sleep 2
-	done
-	echo "$VM1 $vm1_new_state"
-	sleep 2
-	virsh start "$VM2"
-	sleep 1
-	vm2_new_state=$(virsh domstate "$VM2")
-	echo "$VM2 $vm2_new_state"
+        echo "$VM1 is running shutting down"
+        virsh shutdown "$VM1"
+        vm1_new_state=$(virsh domstate "$VM1")
+        until [ "$vm1_new_state" = "$vm_down" ]; do
+                echo "$VM1 $vm1_new_state"
+                vm1_new_state=$(virsh domstate "$VM1")
+                sleep 2
+        done
+        echo "$VM1 $vm1_new_state"
+        sleep 2
+        virsh start "$VM2"
+        sleep 1
+        vm2_new_state=$(virsh domstate "$VM2")
+        echo "$VM2 $vm2_new_state"
 else
-	if [ "$vm2_state" = "$vm_running" ] && [ "$vm1_state" = "$vm_down" ]; then
-		echo "$VM2 is running shutting down"
-		virsh shutdown "$VM2"
-		vm2_new_state=$(virsh domstate "$vm2")
-		until [ "$vm2_new_state" = "$vm_down" ]; do
-			echo "$VM2 $vm2_new_state"
-			vm2_new_state=$(virsh domstate "$VM2")
-			sleep 2
-		done
-		echo "$VM2 $vm2_new_state"
-		sleep 2
-		virsh start "$VM1"
-		sleep 1
-		vm1_new_state=$(virsh domstate "$VM1")
-		echo "$VM1 $vm1_new_state"
-	else
-		echo "$VM1 $vm1_state and $VM2 $vm2_state doesnt match"
-	fi
+        if [ "$vm2_state" = "$vm_running" ] && [ "$vm1_state" = "$vm_down" ]; then
+                echo "$VM2 is running shutting down"
+                virsh shutdown "$VM2"
+                vm2_new_state=$(virsh domstate "$vm2")
+                until [ "$vm2_new_state" = "$vm_down" ]; do
+                        echo "$VM2 $vm2_new_state"
+                        vm2_new_state=$(virsh domstate "$VM2")
+                        sleep 2
+                done
+                echo "$VM2 $vm2_new_state"
+                sleep 2
+                virsh start "$VM1"
+                sleep 1
+                vm1_new_state=$(virsh domstate "$VM1")
+                echo "$VM1 $vm1_new_state"
+        else
+                echo "$VM1 $vm1_state and $VM2 $vm2_state doesnt match"
+        fi
 fi
